44e7265448c63ce0bd46854c36131c7958166657
Fab <mail@fabschneider.com>
2021-12-06 14:51:53 +0100

    Add simple step example

---
+++ Arduino/Examples/SimpleStep/SimpleStep.ino
@@ -0,0 +1,25 @@
+#define enable_1 2
+#define step_1 3
+#define dir_1 4
+
+#define enable_2 5
+#define step_2 6
+#define dir_2 7
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  pinMode(enable_2, OUTPUT);
+  pinMode(step_2, OUTPUT);
+  pinMode(dir_2, OUTPUT);
+
+  digitalWrite(enable_2, LOW);
+  digitalWrite(dir_2, HIGH);
+}
+
+void loop() {
+  digitalWrite(step_2,HIGH);
+  delay(1); 
+  digitalWrite(step_2,LOW);
+  delay(1);
+}
