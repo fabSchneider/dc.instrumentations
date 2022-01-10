7b175845a2d2de98b09af3980ba1aa0fc1285330
Fab <mail@fabschneider.com>
2022-01-10 16:39:22 +0100

    Add random walk

--- Python/Polargraph/util.py
+++ Python/Polargraph/util.py
@@ -9,3 +9,12 @@ def clamp_to_unit(x, y):
         y = y / mag * min(mag, 1)
     
     return x, y
+
+def normalize(x, y):
+    mag = math.sqrt(pow(x, 2) + pow(y, 2))
+
+    if (mag > 0) :
+        x = x / mag
+        y = y / mag
+    
+    return x, y
