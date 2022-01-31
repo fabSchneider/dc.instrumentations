231c7cffa4636d60a051a2c58ebb022032286aec
Fab <mail@fabschneider.com>
2022-01-31 11:41:58 +0100

    Add sequence program

---
+++ Python/Program/sequence.py
@@ -0,0 +1,140 @@
+from serial.serialutil import SerialException
+import sys
+import os
+import keyboard
+import time
+import serial
+import math
+import random
+
+sys.path.append(os.getcwd())
+
+from Polargraph import util
+from Polargraph import plotter
+from Polargraph import gamepad
+from Polargraph import task
+from Gamepad import gamepad_control
+
+# arduino serial settings
+port = 'COM5'
+baudrate = 115200
+
+gpad_y_pressed = False
+gpad_y_down = False
+gpad_b_pressed = False
+gpad_b_down = False
+
+def read_gpad(gpad):
+    global gpad_y_pressed 
+    global gpad_y_down 
+    global gpad_b_pressed 
+    global gpad_b_down 
+
+    gpad_y_down = False
+    gpad_b_down = False
+    if(gpad.Y == 1 and not gpad_y_pressed):
+        gpad_y_down = True
+    if(gpad.B == 1 and not gpad_b_pressed):
+        gpad_b_down = True
+
+    gpad_y_pressed = gpad.Y == 1
+    gpad_b_pressed = gpad.B == 1
+
+def run_circle(task):
+    print("Starting circle")
+    pos = 0.0
+
+    while task._running:
+        pos += 0.03
+        plotter.send_xy(task._arduino, math.sin(pos), math.cos(pos))
+        time.sleep(0.03)
+
+    # send stop
+    plotter.send_xy(task._arduino, 0, 0)
+    print("Stopped circle")
+
+def run_random(task):
+    print("Starting random walk")
+    # dir = random.uniform(-math.pi, math.pi)
+    dir = random.randint(0, 3) * math.pi * 0.5
+
+    sequence = [0.1, 0.3, 0.6, 1, 3, 6]
+
+    start_time = time.time()
+    seq_id = 0
+    seq_dir = 1
+    interval_len = 13.0
+    while task._running:
+        # dir += random.uniform(-math.pi * 1.0, math.pi * 1.0)
+        dir += random.randint(0, 3) * math.pi * 0.5
+        x = math.cos(dir)
+        y = math.sin(dir)
+        plotter.send_xy(task._arduino, x, y)
+        time.sleep(sequence[seq_id])
+        curr_interval = time.time() - start_time
+        if(curr_interval > interval_len):
+            start_time = time.time()
+            if(seq_id == len(sequence) - 1):
+                seq_dir = -1
+            elif(seq_id == 0):
+                seq_dir = 1      
+            seq_id = seq_id + seq_dir
+            print("Starting Generation " + str(seq_id))
+
+    # send stop
+    plotter.send_xy(task._arduino, 0, 0)
+    print("Stopped random walks")
+
+def main():
+    # create the gamepad listener
+    gpad = gamepad.Gamepad()
+
+    try:
+        arduino = serial.Serial(port, baudrate, timeout = 0.003)
+
+        circleTask = task.PolargraphTask(arduino, run_circle)
+        randomTask = task.PolargraphTask(arduino, run_random)
+
+
+        gpad_control = gamepad_control.GampadControl(gpad, arduino)
+        gpad_control.start()
+
+        currTask = gpad_control
+
+        while(not keyboard.is_pressed('esc')):
+            read_gpad(gpad)
+
+            if(gpad_y_down):
+                if(currTask == circleTask):
+                    circleTask.stop()
+                    currTask = gpad_control
+                    currTask.start()
+                else:
+                    currTask.stop()
+                    currTask = circleTask
+                    currTask.start()
+            elif(gpad_b_down):
+                if(currTask == randomTask):
+                    randomTask.stop()
+                    currTask = gpad_control
+                    currTask.start()
+                else:
+                    currTask.stop()
+                    currTask = randomTask
+                    currTask.start()
+                
+            time.sleep(0.01)
+
+        currTask.stop()
+        plotter.send_xy(arduino, 0, 0)
+        arduino.close()    
+        print("Program stopped")
+
+    except SerialException as e:
+        print (e)   
+
+if __name__ == '__main__':   
+   main()
+ 
+
+
