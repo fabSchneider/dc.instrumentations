66df3f2ac6aa6167e5360b9c23802c8d14f855fd
Fabian Schneider <mail@fabschneider.com>
2022-02-14 10:13:17 +0100

    Move time utils to own file

--- Python/presentation.py
+++ Python/presentation.py
@@ -7,24 +7,19 @@ from Programs.random_walk import random_walk
 from Programs.home import home
 from Programs.random_pos import random_brush
 from Programs.camera_feed import camera_feed
+import time_utils
 
 # arduino serial settings
 # port = 'COM5'
 port = '/dev/ttyACM0'
 baudrate = 115200
 
-def min_to_sec(min):
-    return min * 60
-
-def hour_to_sec(h):
-    return h * 3600
-
 if __name__ == '__main__':   
 
     try:
         arduino = serial.Serial(port, baudrate, timeout = 0.003)
         plotter = Plotter(arduino)
-        plotter.run([home, camera_feed, random_walk, random_brush], run_time = hour_to_sec(3))
+        plotter.run([home, camera_feed, random_walk, random_brush], run_time = time_utils.hour_to_sec(3))
     except SerialException as e:
         print(e)
 
