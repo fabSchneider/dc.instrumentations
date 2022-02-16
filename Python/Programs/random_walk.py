6bd83d9eb52ef6d6ec26b3ef90a2698bb8f3f0c7
Fabian Schneider <mail@fabschneider.com>
2022-02-16 09:17:43 +0100

    Reset seed to 999

--- Python/Programs/random_walk.py
+++ Python/Programs/random_walk.py
@@ -21,7 +21,7 @@ def random_walk(task):
 
 def random_walk_sequence(task):
     print("Starting random walk")
-    random.seed(998)
+    random.seed(999)
 
     # dir = random.uniform(-math.pi, math.pi)
     dir = random.randint(0, 3) * math.pi * 0.5
